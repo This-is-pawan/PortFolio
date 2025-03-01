eb97cd49fbff8d1d3d4c1e6664497f6cd23f10ef
pawan <pawanjalandhara@gmail.com>
2025-03-01 13:14:41 +0530

    updateWithMode

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { FaFaceGrin, FaLightbulb, FaMoon } from 'react-icons/fa6'
 
 const DarkMode = () => {
   const [Moon,setSun]=useState(true)
+  const Dark=useRef(document.body)
+  if (!Moon) {
+    Dark.current.classList.add('Light')
+  }
+else{
+  Dark.current.classList.remove('Light')
+}
+ 
   const MakeSun=()=>{
     setSun(!Moon)
+   
   }
   return (
     <>
     <article className='dark_mode' onClick={MakeSun}>
-      {(Moon)?<FaMoon className='mode_moon ' />:<FaFaceGrin className='mode_sun' />}
+      {
+      (Moon)?<FaMoon className={`mode_moon`}/>:<FaFaceGrin className={`mode_sun`} />
+      }
    
-
    
     </article>
     </>
