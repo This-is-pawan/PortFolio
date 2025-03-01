51367d3a3c7d77b827cadfc583a04063d83d0494
pawan <pawanjalandhara@gmail.com>
2025-03-01 14:28:24 +0530

    update

--- src/components/routes/routesLinks/RoutesLinks.jsx
+++ src/components/routes/routesLinks/RoutesLinks.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import { About, HomePage, Project } from "../components/routes/RouteJoining";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const RoutesLinks = () => {
+const [e,setE]=useState(true)
+const Link_color=()=>{
+setE(!e)
+}
   return (
     <>
 <div className="routes">
         <div className="route">
           <article>
-      
-            <Link to='/'>Home</Link>
+          <NavLink to="/" className={({ isActive }) => (isActive ? "yellow" : "blue")}>
+        Home
+      </NavLink>
+            {/* <Link to='/' onClick={Link_color}className={(e)?'yellow':'red'}>Home</Link> */}
           </article>
           <article>
-            <Link to='about'>About</Link>
-    
+          <NavLink to="/about" className={({ isActive }) => (isActive ? "yellow" : "blue")}>
+        About
+      </NavLink>
           </article>
           <article>
-            <Link to='/project'>project</Link>
+          <NavLink to="/project" className={({ isActive }) => (isActive ? "yellow" : "blue")}>
+        Project
+      </NavLink>
           
           </article>
         </div>
